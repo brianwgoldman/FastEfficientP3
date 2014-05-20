e355b83a7f895bd2e50035c4aa4c9a3843e0856c
Goldman <brianwgoldman@acm.org>
2014-05-20 16:26:55 -0400

    Popless implementation

---
+++ src/Popless.cpp
@@ -0,0 +1,44 @@
+// Implements the Parameter-less GA method of population
+// parameter removal.
+
+#include "Popless.h"
+#include "OptimizationCollection.h"
+
+Popless::Popless(Random& _rand, Evaluator& _evaluator, Configuration& _config)
+    : Optimizer(_rand, _evaluator, _config) {
+  largest_size = config.get<int>("pop_size");
+  // copy the configuration and set up the sub_optimizer
+  Configuration lesser(config);
+  auto optimizer_method = config.get<optimize::pointer>("sub_optimizer");
+  pops.push_back(optimizer_method(rand, evaluator, lesser));
+  generations.push_back(0);
+  up_next = 0;
+}
+
+bool Popless::iterate() {
+  // performs an iteration of one population
+  pops[up_next]->iterate();
+  generations[up_next]++;
+  if (generations[up_next] % 2 == 0) {
+    up_next++;
+    // creates new population as necessary
+    if (up_next >= pops.size()) {
+      // set up the new population size
+      Configuration lesser(config);
+      largest_size *= 2;
+      lesser.set("pop_size", largest_size);
+      auto optimizer_method = config.get<optimize::pointer>("sub_optimizer");
+      pops.push_back(optimizer_method(rand, evaluator, lesser));
+      generations.push_back(0);
+    }
+  } else {
+    up_next = 0;
+  }
+  // Effectively terminates populations after they have performed
+  // too many iterations
+  while (generations[up_next] >= length) {
+    up_next++;
+  }
+  return true;
+}
+
---
+++ src/Popless.h
@@ -0,0 +1,34 @@
+// Brian Goldman
+
+// Implementation of the Parameter-less GA's
+// method for removing the population size parameter
+// Designed to match the variant in the paper:
+// "Parameter-Less Hierarchical BOA"
+// by M. Pelikan and T. K. Lin
+// Takes a configurable sub_optimizer which
+// is kept in a collection of populations, each using a
+// different population size.
+
+#ifndef POPLESS_H_
+#define POPLESS_H_
+
+#include "Optimizer.h"
+
+// Inherits and implements the optimizer
+class Popless : public Optimizer {
+ public:
+  Popless(Random& _rand, Evaluator& _evaluator, Configuration& _config);
+  bool iterate() override;
+  create_optimizer(Popless);
+ private:
+  // collections of populations
+  vector<shared_ptr<Optimizer>> pops;
+  // records how many iterations each population has performed
+  vector<size_t> generations;
+  // keeps track of the size of the current population
+  size_t largest_size;
+  // which population gets to perform the next iteration
+  size_t up_next;
+};
+
+#endif /* POPLESS_H_ */
