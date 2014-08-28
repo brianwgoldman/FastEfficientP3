518b35669ee002955793e68237649c05093bfce6
Brian W. Goldman <brianwgoldman@acm.org>
2014-08-27 21:01:04 -0400

    New makefiles for a new Eclipse. Identical in flags to the old ones.

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -62,7 +62,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -O0 -g3 -pg -pedantic -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -std=c++11 -O0 -g3 -pg -pedantic -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
